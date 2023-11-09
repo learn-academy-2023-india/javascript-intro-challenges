dac3bed7259971a014dc36b9c0dd0b02e1742d3e
xdfripp@gmail.com <learnacademy@LEARNs-MacBook-Air-4.local>
2023-11-09 15:10:06 -0800

    answered questions now switching

---
+++ conditionals-guillermo-xavier.js
@@ -0,0 +1,22 @@
+// Is 34 divided by 3 greater than 67 divided by 2?
+console.log(34 / 3 > 67 / 2)
+// Does 5 evaluate to the same as "5"?
+console.log(5 == "5")
+// Does 5 strictly equal "5"?
+console.log(5 === "5")
+// Does !3 strictly equal 3?
+console.log(3 !== 3)
+// Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+console.log( "LEARN".length === 5 && "Student".length === 7)
+// Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+// Does "LEARN" contain the subset "RN"?
+// Does "LEARN" contain the subset "rn"?
+// Does "LEARN"[0] strictly equal "l"?
+// Modify the code from the previous question to return true.
+// Write a conditional statement for the following prompts. Make sure you try different options and change the variables to ensure properly working code.
+
+// Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
+// Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+// Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+// Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".
+// Write a statement that takes a variable of a number and logs whether the number is odd, even, or zero.
