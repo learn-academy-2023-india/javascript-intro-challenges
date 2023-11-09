543018e07542e8ed3e557a4264b5cd201969a945
tuguldur <tmgl8808@gmail.com>
2023-11-08 17:02:07 -0800

    Javascript chalanges for Tuguldur and Soza

---
+++ Javascript-TB-RS.js
@@ -0,0 +1,21 @@
+//var mynumber = 2
+//console.log(mynumber + 5)
+// Write the code that will log the output of 34 added to 71.
+34+71
+// Write the code that will log the output of 67 subtracted from 123.
+123-67
+// Write the code that will log the output of 56 multiplied by 23.
+56*23
+// Write the code that will log the output of 45 divided by 5.
+45/5
+// Write the code that will log the output of 5 to the power of 7.
+5**7
+// Write the code that will log the whole number remainder of 33 divided by 6.
+33%6
+// Write the code that will log the length of a string containing your name.
+("Tuguldur".length)
+// Write the code that will log whether your string includes the letter "e"?
+
+// Write the code that will log the character at the first index of the string.
+
+// Write the code that will log the string in all uppercase letters.
