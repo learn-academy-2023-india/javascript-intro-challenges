64f82166c7059a477e9e90a40d314343dd2dda52
Mamba240824 <leo.ramos70@yahoo.com>
2023-11-09 15:44:10 -0800

    destroying challenges

--- conditionals-leo-keoni.js
+++ conditionals-leo-keoni.js
@@ -1,10 +1,22 @@
 // Is 34 divided by 3 greater than 67 divided by 2?
+console.log((34 / 3) > (67 /2 ))
 // Does 5 evaluate to the same as "5"?
+console.log( 5 == "5")
 // Does 5 strictly equal "5"?
+console.log( 5 === "5" )
 // Does !3 strictly equal 3?
+console.log( 3 === !3)
 // Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+var learn = "LEARN"
+var Student = "student" 
+console.log((learn.length === 5) && (Student.length === 7))
 // Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+console.log((learn.length === 5) || (Student.length === 10))
 // Does "LEARN" contain the subset "RN"?
+console.log(learn.indexOf(4,5) === ["RN"])
 // Does "LEARN" contain the subset "rn"?
+console.log(learn.indexOf(4,5) === ["rn"])
 // Does "LEARN"[0] strictly equal "l"?
+console.log(learn[0] === "l")
 // // Modify the code from the previous question to return true.
+console.log(learn[0] === "L")
