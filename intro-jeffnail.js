0545dc6b8de214de68b85b3846c7712767b77b10
JeffNailisKANGtheCoder <KANGtheCoder@outlook.com>
2023-11-08 17:14:23 -0800

    logic, boolians, and number script

---
+++ intro-jeffnail.js
@@ -0,0 +1,17 @@
+let todoList = [
+    { task: "Complet JavaScript Challenges", isCompleted: true },
+{ task: "Save, push to github org for India Cohort, reconcile diff", isCompleted: true },
+{ task: "help others complet their challenge if time permits", isCompleted: False }
+    ]
+
+    let a = 5
+    let c = 10
+    let sum = a += c
+    let difference = a -= c
+    let product = a *= c
+    let quotient = a / c
+
+let (isLogged) = true 
+
+if (isLogged) {console.log("User is logged in.");
+} else { console.log("User is not logged in.");
