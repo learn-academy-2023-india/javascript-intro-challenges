4a4d820a4f4f79523c2c77610aad11c2210d21c2
Mamba240824 <leo.ramos70@yahoo.com>
2023-11-09 14:53:13 -0800

    added the challenges

---
+++ conditionals-leo-keoni.js
@@ -0,0 +1,10 @@
+// Is 34 divided by 3 greater than 67 divided by 2?
+// Does 5 evaluate to the same as "5"?
+// Does 5 strictly equal "5"?
+// Does !3 strictly equal 3?
+// Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+// Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+// Does "LEARN" contain the subset "RN"?
+// Does "LEARN" contain the subset "rn"?
+// Does "LEARN"[0] strictly equal "l"?
+// // Modify the code from the previous question to return true.
