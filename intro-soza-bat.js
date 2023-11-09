906331d95e36764626cd3c512d6deb50b7bd032c
RobertSarsoza <sarsoza@gmail.com>
2023-11-08 16:58:59 -0800

    saving

---
+++ intro-soza-bat.js
@@ -0,0 +1,15 @@
+
+console.log (34 + 71)
+// Write the code that will log the output of 67 subtracted from 123.
+console.log(123-67)
+// Write the code that will log the output of 56 multiplied by 23.
+console.log(56*23)
+// Write the code that will log the output of 45 divided by 5.
+console.log(45/5)
+// Write the code that will log the output of 5 to the power of 7.
+console.log(5**7)
+// Write the code that will log the whole number remainder of 33 divided by 6.
+console.log(33%6)
+// Write the code that will log the length of a string containing your name.
+var name1 ="soza"
+console.log(name1.length)
