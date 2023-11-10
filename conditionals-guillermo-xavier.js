ad3aee160df520d3082ea472df51257fc25b9595
xdfripp@gmail.com <learnacademy@LEARNs-MacBook-Air-4.local>
2023-11-09 17:25:14 -0800

    added answers

--- conditionals-guillermo-xavier.js
+++ conditionals-guillermo-xavier.js
@@ -21,10 +21,25 @@ console.log ("LEARN"[0] === "L")
 
 // Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
 
-// Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
 
+// Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+var hungry = "true"
+if(hungry === "true") {
+    console.log('eat food')
+} else if(hungry === "false") {
+    console.log('keep coding')
+}
 // Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+var trafficlight = "green" 
+if(trafficlight === "green"){
+    console.log('go')
+} else if(trafficlight === "yellow") {
+    console.log('slow down')
+} else if(trafficlight === "red") {
+    console.log('stop')
+}
 
 // Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".
+var 
 
 // Write a statement that takes a variable of a number and logs whether the number is odd, even, or zero.
