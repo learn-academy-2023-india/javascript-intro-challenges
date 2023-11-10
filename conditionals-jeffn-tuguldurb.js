a7572902e97988f558f424a3d9034ab0370c1b82
tuguldur <tmgl8808@gmail.com>
2023-11-09 16:38:33 -0800

    added two more code

--- conditionals-jeffn-tuguldurb.js
+++ conditionals-jeffn-tuguldurb.js
@@ -4,8 +4,23 @@ console.log (5 ==='5')
 console.log (!3===3)
 console.log ("LEARN".length===5 && "Student".length===7)
 console.log ("LEARN".length===5 || "Student".length===10)
+var price = 99
+if( price < 100){
+  console.log("in budget")}
+var hungry = eat
+if( hungry === eat){
+    console.log( eatfood) }
+    else if(hungry !== eat ){console.log( keepcoding)}
+        
+    
+
+
+
+
+
 const LEARN = "RN"
 if (LEARN === "RN") {
     console.log("True")}
     if (LEARN !== "rn") {
         console.log("false")}
+        
