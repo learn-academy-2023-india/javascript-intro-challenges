14b581ac5ab0e094dfaf5121c340a7465b90a92d
JeffNailisKANGtheCoder <KANGtheCoder@outlook.com>
2023-11-09 16:03:34 -0800

    conditional challenge halfway done

---
+++ conditionals-jeffn-tuguldurb.js
@@ -0,0 +1,11 @@
+console.log (34/3 > 67/2)
+console.log (5=='5')
+console.log (5 ==='5')
+console.log (!3===3)
+console.log ("LEARN".length===5 && "Student".length===7)
+console.log ("LEARN".length===5 || "Student".length===10)
+const LEARN = "RN"
+if (LEARN === "RN") {
+    console.log("True")}
+    if (LEARN !== "rn") {
+        console.log("false")}
