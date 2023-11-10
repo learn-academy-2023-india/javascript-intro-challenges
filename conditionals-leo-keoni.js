2ad044a46beaad0c3104479f4fec109bb7c158ba
Mamba240824 <leo.ramos70@yahoo.com>
2023-11-09 16:49:15 -0800

    completion

--- conditionals-leo-keoni.js
+++ conditionals-leo-keoni.js
@@ -20,3 +20,51 @@ console.log(learn.indexOf(4,5) === ["rn"])
 console.log(learn[0] === "l")
 // // Modify the code from the previous question to return true.
 console.log(learn[0] === "L")
+
+// Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
+var item = 50 
+var price = 100
+if(item < price  ){
+   console.log("in budget")
+}
+// Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+var hungry = false
+if (hungry){
+    console.log ("eat food")
+}
+else {
+    console.log("keep coding")
+    
+}
+// Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+var trafficLight = "red"
+if(trafficLight === "green") {
+    console.log('go')
+} else if( trafficLight === "yellow"){
+    console.log("slow down")
+} else {
+    console.log("stop")
+}
+// Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".
+const leo = 60
+const keoni = 60
+if(keoni > leo){
+    console.log(keoni)
+}
+else if (keoni === leo){
+    console.log("the numbers are the same")
+}
+else {(keoni < leo)
+    console.log(leo)
+}
+// Write a statement that takes a variable of a number and logs whether the number is odd, even, or zero.
+const number = 5
+    if (number === 0) {
+      console.log("The number is zero.")
+    } else if (number % 2 === 0) {
+      console.log("The number is even.")
+    } else {
+      console.log("The number is odd.")
+    }
+
+  
