4e165d45e387b657c53db386f4434282d016635a
xdfripp@gmail.com <learnacademy@LEARNs-MacBook-Air-4.local>
2023-11-09 16:49:39 -0800

    added more answers to the challenge

--- conditionals-guillermo-xavier.js
+++ conditionals-guillermo-xavier.js
@@ -7,9 +7,9 @@ console.log(5 === "5")
 // Does !3 strictly equal 3?
 console.log(3 !== 3)
 // Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
-console.log( "LEARN".length === 5 && "Student".length === 7)
+console.log("LEARN".length === 5 && "Student".length === 7)
 // Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
-console.log( "LEARN".length === 5 || "Student".length === 10)
+console.log("LEARN".length === 5 || "Student".length === 10)
 // Does "LEARN" contain the subset "RN"?
 console.log ("LEARN".indexOf(4,5) === ["RN"])
 // Does "LEARN" contain the subset "rn"?
@@ -18,12 +18,13 @@ console.log ("LEARN".indexOf(4,5) === ["rn"])
 console.log ("LEARN"[0] === "l")
 // Modify the code from the previous question to return true.
 console.log ("LEARN"[0] === "L")
-// Write a conditional statement for the following prompts. Make sure you try different options and change the variables to ensure properly working code.
-
 
 // Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
 
 // Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+
 // Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+
 // Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".
+
 // Write a statement that takes a variable of a number and logs whether the number is odd, even, or zero.
