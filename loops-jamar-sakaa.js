66412638de81fdd6229e7863bec622b7098568f5
jwat5623 <jamar34watson56@gmail.com>
2023-11-13 15:09:35 -0800

    intro loops challenge

---
+++ loops-jamar-sakaa.js
@@ -0,0 +1,18 @@
+// Create a for loop that logs each number from 1 - 20.
+
+// Create a for loop that logs every other number from 1 - 20.
+// Create a for loop that logs the result of each number from 1 - 20 tripled.
+// Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD".
+// Expected output: ODD, 2, ODD, 4, ODD, 6 ...etc
+// Consider this variable:
+
+// const nums = [3, 57, -9, 20, 67]
+// Create the code that will log the largest number from the array.
+// Create the code that will log the smallest number from the array.
+// Create the code that will log the remainder of each number when divided by 2.
+// Expected output: 1, 1, -1, 0, 1
+// Consider this variable:
+
+// const myString = "learn student"
+// Create the code that will log the number of times the letter "e" occurs in the string.
+// Create the code that will log every other character in the string.
