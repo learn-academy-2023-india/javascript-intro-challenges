3787e9b6fbb463ad6fda07280ee133d3fbbbd918
RobertSarsoza <sarsoza@gmail.com>
2023-11-13 14:56:39 -0800

    first challenges soza was driver

---
+++ arrays-soza-bat.js
@@ -0,0 +1,14 @@
+var groceryList = ["chips", "dip", "cookies"]
+// Write the code that will add "soda" to the end of the original array.
+console.log(groceryList.push("soda"))
+console.log(groceryList)
+
+// Write the code that will add "granola" to the end of the array without altering the original array.
+console.log(groceryList.indexOf("soda"))
+var sodaIndex = groceryList.indexOf("soda")
+console.log(sodaIndex)
+console.log(groceryList.indexOf("granola"))
+console.log(groceryList)
+
+// Write the code that will return a subset of the array containing only "chips" and "dip".
+// Write the code that will add "beans" to the "chips" and "dip" array.
