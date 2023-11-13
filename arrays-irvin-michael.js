af20c5731f5e1a9de65b2bb5ec562b7d982a1de8
irvinmoore <irvin.moore7@gmail.com>
2023-11-13 15:46:14 -0800

    updates to arrays

---
+++ arrays-irvin-michael.js
@@ -0,0 +1,22 @@
+// Write the code that will add "soda" to the end of the original array.
+var groceryList = ["chips", "dip", "cookies"]
+    groceryList.push("soda")
+    console.log(groceryList)
+// Write the code that will add "granola" to the end of the array without altering the original array.
+groceryList.pop("granola")
+console.log(groceryList)
+// Write the code that will return a subset of the array containing only "chips" and "dip".
+groceryList.
+console.log("chip" and "dip")
+// Write the code that will add "beans" to the "chips" and "dip" array.
+// var numbers = [2, 4, 6, 8, 10]
+// Write the code that will add the number 0 to the beginning of the array.
+// Write the code that will add the number 12 to the end of the array.
+// Write the code that will remove the first number from the array.
+// Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+// Consider the variable:
+
+// var numSet = [2, 13, 6, 8, 4, 2]
+// Write the code that finds the index of the first appearance of the number 2.
+// Write the code that finds the index of the last appearance of the number 2.
+// Write the code that returns the number at the third index.
