2bff8e8c2de49895c933a818dd00aadb262f9b9a
Sakaa4Short <klanyenek@gmail.com>
2023-11-13 16:38:01 -0800

    arrays answers updated

--- arrays-sakaa-jamar.js
+++ arrays-sakaa-jamar.js
@@ -1,23 +1,19 @@
 // Copy the challenges into your JavaScript file. Comment out the instructions and code the solution to each problem beneath the prompt.
 
 // Consider the variable:
-
 var groceryList = ["chips", "dip", "cookies"]
-groceryList.push("soda")
+// Write the code that will add "soda" to the end of the original array.
+// groceryList.push("soda")
+// console.log(groceryList)
+// Write the code that will add "granola" to the end of the array without altering the original array.
+console.log(groceryList.concat("granola"))
 console.log(groceryList)
-
-groceryList.push("granola")
+// Write the code that will return a subset of the array containing only "chips" and "dip".
+groceryList.pop()
 console.log(groceryList)
-
-console.log(groceryList[0])
-console.log(groceryList[1])
-
+// Write the code that will add "beans" to the "chips" and "dip" array.
 groceryList.push("beans")
 console.log(groceryList)
-// Write the code that will add "soda" to the end of the original array.
-// Write the code that will add "granola" to the end of the array without altering the original array.
-// Write the code that will return a subset of the array containing only "chips" and "dip".
-// Write the code that will add "beans" to the "chips" and "dip" array.
 // Consider the variable:
 
 // var numbers = [2, 4, 6, 8, 10]
