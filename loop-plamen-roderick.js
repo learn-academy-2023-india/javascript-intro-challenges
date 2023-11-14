8066a95f7bdf699f425ec698a6e2fbbfe2a7aeff
RoderickVongsa <roderick.vongsa@gmail.com>
2023-11-13 16:32:31 -0800

    Stuck on Largest number in array question

--- loop-plamen-roderick.js
+++ loop-plamen-roderick.js
@@ -24,21 +24,44 @@
 
 // Create a for loop that logs the result of each number from 1 - 20 tripled.
 
-for (let i = 1; i <= 20; i++) {
-    let result =i*3;
-    console.log(i) }
+//for (let i = 1; i <= 20; i++) {
+  //  let result =i*3;
+   // console.log(i) }
 
 
 // Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD".
-
-
-
 // Expected output: ODD, 2, ODD, 4, ODD, 6 ...etc
 // Consider this variable:
-
 // const nums = [3, 57, -9, 20, 67]
+
+// for (let i = 1; i <= 20; i++) {
+//     if(i % 2 === 0) {
+//     console.log(i) }; // Even numbers log here
+//     } else {
+//     console.log("ODD"); //Log "ODD" here for odd numbers
+//     }
+
+
+
+
+
 // Create the code that will log the largest number from the array.
+// let highestNumber = 20;
+
+// for (let i = 1; i < 20; i++) {
+//  if (i > highestNumber) 
+//     highestNumber = i;
+
+//     console.log("Highest number is", highestNumber);
+// }
+// code reads highest number is 20
+
+
+
 // Create the code that will log the smallest number from the array.
+
+
+
 // Create the code that will log the remainder of each number when divided by 2.
 // Expected output: 1, 1, -1, 0, 1
 // Consider this variable:
