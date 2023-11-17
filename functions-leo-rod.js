b3b0c835d8d02b7ef411cd5372d7118ce05af5f2
Mamba240824 <leo.ramos70@yahoo.com>
2023-11-17 10:43:14 -0800

    challenges update

--- functions-leo-rod.js
+++ functions-leo-rod.js
@@ -27,18 +27,59 @@ const triple = (number2) => {
    }
    console.log(triple(21))
 // Write a function named multiply that takes two numbers as arguments and returns the result of the numbers multiplied together.
-const multiply = (number3, number4, number5) => {
-    var number5 = number3 * number4
-    return `${number3} * ${number4} = ${number5}`
-   }
-   console.log(multiply(2),(2),(number5))
+const multiply = (num1, num2) => {
+    return num1 * num2
+  }
+  
+  console.log(multiply(10, 4))
+  // output: 40
+  
+
+// const multiply = (number3, number4, number5) => {
+//     var number5 = (number3 * number4)
+//     return `${number5}`
+//    }
+//    console.log(multiply(2),(2),)
 // Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+
+const divisibleBy = (numm1, numm2) => {
+    if (numm1 / numm2)
+    return `${numm1} is evenly divisible by ${numm2}.`
+  }
+  console.log(divisibleBy(10, 5))
 
 // Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
 
 // Write a function named isLonger that takes two strings as arguments and returns the string that contains the most characters.
 
+const isLonger = (words1, words2) => {
+    if (words1 < words2)
+    return `${words2}.`
+    else if (words1 > words2)
+    return `${words1}`
+    else 
+    return `They're even`
+  }
+
+  console.log(isLonger("you", "no"))
+
+
 // Write a function named greaterNum that takes two numbers as arguments and returns whichever number is the greater (higher) number.
+
+const greaterNum = (nummm1, nummm2) => {
+    if (nummm1 < nummm2)
+    return `${nummm2}.`
+    else if (nummm1 > nummm2)
+    return `${nummm1}`
+    else 
+    return `They're even`
+  }
+  console.log(greaterNum(70, 70))
 
 // Write a function named yelling that takes a string as an argument and return the string in all uppercase case letters.
 
+const yelling = (scream) => {
+
+    return `${scream.toUpperCase()}`
+  }
+  console.log(yelling("i love you!"))
